30736f795cb5d64ac793cca78c22bd4ff6c69f03
Unknown-XK <73538457+Unknown-XK@users.noreply.github.com>
2020-11-03 16:26:30 +0100

    Add files via upload

--- XK-B64.py
+++ XK-B64.py
@@ -1,5 +1,4 @@
-
-#XK-B64
+#XK-B64 v2.4.8
 
 import base64, sys, time
 
@@ -7,40 +6,46 @@ def type(txt):
   for char in txt:
     sys.stdout.write(char)
     sys.stdout.flush()
-    time.sleep(0.05)
+    time.sleep(0.02)
     
 print("\033[1;31;40m")
 type("Base64 Encoder/Decoder made by Unknown-XK\n")
 print("\033[1;37;40m")
-type("Hello to XK-B64...\n\n")
+type("Hello to XK-B64...\n")
 
-type("Type 'e' for encoding, 'd' for decoding:")
+def xk():
+  if True:
+    time.sleep(1)
+    print("\033[1;31;40m")
+    type("Type 'e' for encoding, 'd' for decoding or 'x' for exit:")
+    x = input()
+    print("\033[1;37;40m")
+  if x == "e":
+    type("Enter the Message:\n\n")
+    msg = input()
+    print()
+    msg = msg.encode("ascii")
+    b64 = base64.b64encode(msg)
+    b64m = b64.decode("ascii")
+    type("Your encoded message is:\n\n")
+    type(b64m)
+    print()
+  elif x == "d":
+    type("Enter the encoded message:\n\n")
+    b64m = input()
+    print()
+    b64m = b64m.encode("ascii")
+    b64 = base64.b64decode(b64m)
+    msg = b64.decode("ascii")
+    type("Your decoded message is:\n\n")
+    type(msg)
+    print()
+  elif x == "x":
+    type("Process ended...\n")
+    print("\033[0m")
+    exit()
+  else:
+    type("Invalid input.\n")
 
-x = input()
-
-print()
-if x == "e":
-  type("Enter the Message:\n\n")
-  msg = input()
-  print()
-  msg = msg.encode("ascii")
-  b64 = base64.b64encode(msg)
-  b64m = b64.decode("ascii")
-  type("Your encoded message is:\n\n")
-  type(b64m)
-elif x == "d":
-  type("Enter the encoded message:\n\n")
-  b64m = input()
-  print()
-  b64m = b64m.encode("ascii")
-  b64 = base64.b64decode(b64m)
-  msg = b64.decode("ascii")
-  type("Your decoded message is:\n\n")
-  type(msg)
-else:
-  type("Invalid input.")
-  
-
-print("\n")
-type("Process ended...\n")
-print("\033[0m")
+while True:
+  xk()
